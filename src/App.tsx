c67bcc7bd31723e864c88890f59329cb4f14c9b3
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-03 09:14:39 -0300

    Verificando movimentos 1

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,25 @@ const App = () => {
         return () => clearInterval(timer);
     }, [playing, timeElapsed]);
 
+    useEffect(() => {
+        if (shownCount === 2) {
+            let opened = gridItems.filter(item => item.shown === true);
+            if (opened.length === 2) {
+                if (opened[0].item === opened[1].item) {
+                    let tmpGrid = [...gridItems];
+                    for (let i in tmpGrid) {
+                        if (tmpGrid[i].shown) {
+                            tmpGrid[i].permanentShown = true;
+                            tmpGrid[i].shown = false;
+                        }
+                    }
+                    setGridItems(tmpGrid);
+                    setShownCount(0);
+                }
+            }
+        }
+    }, [shownCount, gridItems])
+
     const resetAndCreateGrid = () => {
         // Passo 1 - resetar o jogo
         setTimeElapsed(0);
@@ -69,7 +88,7 @@ const App = () => {
         if (playing && index !== null && shownCount < 2) {
             let tmpGrid = [...gridItems];
 
-            if(tmpGrid[index].permanentShown === false && tmpGrid[index].shown === false) {
+            if (tmpGrid[index].permanentShown === false && tmpGrid[index].shown === false) {
                 tmpGrid[index].shown = true;
                 setShownCount(shownCount + 1);
             }
